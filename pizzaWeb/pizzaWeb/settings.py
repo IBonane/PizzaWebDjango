d7f8e7a18c5fe6af51b52d37287b6f00bc3a4226
Ibonane <bonanedjimbala@gmail.com>
2021-10-24 00:04:05 +0200

    Pizza web v1

--- pizzaWeb/pizzaWeb/settings.py
+++ pizzaWeb/pizzaWeb/settings.py
@@ -12,20 +12,22 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = environ.Env()
+environ.Env.read_env(env_file=str(BASE_DIR / "pizzaWeb"/".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@(bhx00j(xo-@p%$c&6ty_@aoi8f+p8y+hk5%2%q%#tdsbhle_'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'menu',
+    'main',
 ]
 
 MIDDLEWARE = [
